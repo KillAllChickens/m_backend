818495dff413a87d8212c3a4ac4871e505e14a35
KillAllChickens <admin@killallchickens.org>
2025-12-11 20:03:36 -0600

    UPD: Cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/KillAllChickens/m_backend/routes"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
@@ -12,6 +13,12 @@ func main() {
 	// }
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+        AllowOrigins: []string{"*"},
+        AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+    }))
+
 
 	// Mount route groups
 	routes.IndexRoutes(app)
